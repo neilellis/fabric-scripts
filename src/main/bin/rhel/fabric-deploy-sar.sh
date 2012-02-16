61aca14c10c30354e2344dd806bcba5a49b9c3e6
Neil Ellis <neilellis@cazcade.com>
2012-02-16 00:14:35 +0000

    Fixes for both scripts.

--- src/main/bin/rhel/fabric-deploy-sar.sh
+++ src/main/bin/rhel/fabric-deploy-sar.sh
@@ -14,23 +14,24 @@ rm -rf /tmp/sar-tmp
 mkdir /tmp/sar-tmp
 cd /tmp/sar-tmp
 jar -xf $TARGET
-cat > ./sar.repos << EOF
-index.name: sar
-sar;1:  http://localhost:9985/sar/sar.xml
-EOF
 
 $FABRIC_HOME/bin/posh << EOF
 echo Indexing SAR
 nim:index sar:1 .
 echo Serving SAR as OBR
-poshx:sfs 9985 sar .
+httpURL= sfs 9985 sar .
 nim:policy -R osgi.resolved.bundle/slf4j.api osgi.active.bundle/com.paremus.util.logman
 log-config -c
-fibre --fabric-name=$FABRIC_NAME
+log-config -l debug
+echo Starting Management Fibre
+fibre -I --fabric-name=$FABRIC_NAME
 fabric:connect
-fabric:repos -lmc http://localhost:9985/sar/sar.repos
-fabric:repos -l http://localhost:9985/sar/sar.xml
+echo Loading Bundles
+fabric:repos -lc \$httpURL/sar.xml
+echo Reading System
 fabric:import -f META-INF/system.xml
-
+echo Deploying System
 fabric:deploy $2
 EOF
+
+
--- src/main/bin/rhel/fabric-validate-sar.sh
+++ src/main/bin/rhel/fabric-validate-sar.sh
@@ -14,20 +14,13 @@ rm -rf /tmp/sar-tmp
 mkdir /tmp/sar-tmp
 cd /tmp/sar-tmp
 jar -xf $TARGET
-cat > ./sar.repos << EOF
-index.name: sar
-sar;1:  http://localhost:9985/sar/sar.xml
-EOF
 
 $FABRIC_HOME/bin/posh << EOF
-echo Indexing ...
-nim:index sar:1 .
-echo Loading ...
-poshx:sfs 9985 sar .
 nim:policy -R osgi.resolved.bundle/slf4j.api osgi.active.bundle/com.paremus.util.logman
+index sar:0 .
+repos -l sar.xml
 log-config -c
 log-config -l info
-nim repos -l http://localhost:9985/sar/sar.xml
 repos
 log-config -l debug
 echo Validating ...
